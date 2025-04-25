b3e0dd52c72ef58f83208837907af71fa1e94343
Matheus Lira <admin@MacBook-Air.local>
2025-04-25 16:43:55 -0300

    add styles, validacao, envio de formulario

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,16 +3,18 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="min-h-screen grid-cols-2">
-      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between"></div>
-      <div className="flex items-center gap-3 text-lg text-foreground">
-        <Store className="h-5 w-5" />
-        <span className="font-semibold">delivery.shop</span>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Store className="h-5 w-5" />
+          <span className="font-semibold">delivery.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do Parceiro &copy; delivery.shop - {new Date().getFullYear()}
+        </footer>
       </div>
-      <footer className="text-sm">
-        Painel do Parceiro &copy; delivery.shop - {new Date().getFullYear()}
-      </footer>
-      <div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
