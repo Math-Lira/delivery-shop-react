009498c011738aac8d269408564cfb51583d9bd3
Mathe-Lira <matheusliraa2020@gmail.com>
2025-04-26 23:06:26 -0300

    header e themetoogle

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={'outline'}
+          className="flex items-center gap-2 select-none"
+        >
+          Delivery Shop
+          <ChevronDown className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Matheus Lira</span>
+          <span className=" text-xs font-normal text-muted-foreground">
+            matheusliraa2020@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="w-4 h-4 mr-2" />
+          <span>Perfil da Loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+import { Separator } from '@radix-ui/react-separator'
+import { Home, Store, UtensilsCrossed } from 'lucide-react'
+
+import { AccountMenu } from './account-menu'
+import { Navlink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Store className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <Navlink to={'/'}>
+            <Home className="h-4 w-4" />
+            Inicio
+          </Navlink>
+          <Navlink to={'/orders'}>
+            <UtensilsCrossed className="h-4 w-4" />
+            Pedidos
+          </Navlink>
+        </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
+      </div>
+    </div>
+  )
+}
