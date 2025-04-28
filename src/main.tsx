c40c60d2a2fb8f0d29b106cebb476e9c80bc950b
Matheus Lira <admin@MacBook-Air.local>
2025-04-28 15:03:24 -0300

    add listagem e pedidos

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import './index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 
 import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <HelmetProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HelmetProvider>,
 )
