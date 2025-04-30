7af3dc6fdc2f1a04616a7e0463a07c0f931abba1
Matheus Lira <admin@MacBook-Air.local>
2025-04-30 16:59:02 -0300

    integrando o back-end

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 
-import { App } from './App'
+import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
