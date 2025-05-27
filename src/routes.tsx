4e5810773bf7a7b84782b0f6b5efbabcbe3a0541
Mathe-Lira <matheusliraa2020@gmail.com>
2025-05-27 11:06:12 -0300

    add tratativa de erro que acontece na aplicação

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SingIn } from './pages/auth/sign-in'
 import { SingUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
@@ -25,5 +26,9 @@ export const router = createBrowserRouter([
       { path: '/sign-in', element: <SingIn /> },
       { path: '/sign-up', element: <SingUp /> },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
